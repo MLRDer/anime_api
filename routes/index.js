33ba0b2ff8d5e7f5548faf47d21469086993db32
user <user@DESKTOP-T2KLMGU>
2021-01-02 21:10:54 +0500

    index router fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const animeRouter = require("./animes");
+const collectionRouter = require("./collections");
 const uploadRouter = require("./uploads");
 
 module.exports = (app) => {
     app.use("/api/animes", animeRouter);
     app.use("/api/uploads", uploadRouter);
+    app.use("/api/collections", collectionRouter);
 };
