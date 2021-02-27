4cd8dceec91b2d6d9e6a26e404ae4b22504b7f38
Nuriddin Muhammadjanov <nmuhammadjanov@gmail.com>
2021-02-27 19:33:18 +0500

    V3 - docs scheme auto detection fixed

--- docs/middleware.js
+++ docs/middleware.js
@@ -1,5 +1,5 @@
 module.exports = (docs) => (req, _, next) => {
-    docs.schemes = req.secure ? ['https'] : ['http'];
+    docs.schemes = [req.protocol];
     docs.host = req.get('host');
     req.swaggerDoc = docs;
     next();
