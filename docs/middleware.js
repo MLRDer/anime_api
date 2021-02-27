31eec6a107c13c6562c3d23b38747adc440219b4
Nuriddin Muhammadjanov <nmuhammadjanov@gmail.com>
2021-02-27 20:21:19 +0500

    V3 - get subtitle sources added

--- docs/middleware.js
+++ docs/middleware.js
@@ -1,6 +1,13 @@
+function getProtocol(req) {
+    var proto = req.connection.encrypted ? 'https' : 'http';
+    // only do this if you trust the proxy
+    proto = req.headers['x-forwarded-proto'] || proto;
+    return proto.split(/\s*,\s*/)[0];
+}
+
 module.exports = (docs) => (req, _, next) => {
-    console.log(req.connection.encrypted);
-    docs.schemes = req.connection.encrypted ? ['https'] : ['http'];
+    console.log(getProtocol(req));
+    docs.schemes = [getProtocol(req)];
     docs.host = req.get('host');
     req.swaggerDoc = docs;
     next();
