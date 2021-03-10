e50debaed141469171e4a43bbb8ac069b9e6d58b
MLRDer <r.qirgizboyev@mail.ru>
2021-03-10 15:04:16 +0500

    fix

--- controllers/v2/hdrezka.js
+++ controllers/v2/hdrezka.js
@@ -1,11 +1,11 @@
-require('dotenv/config');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const FormData = require('form-data');
-const catchAsync = require('../../utils/catchAsync');
-const translators = require('../../constants/translator');
+require("dotenv/config");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const FormData = require("form-data");
+const catchAsync = require("../../utils/catchAsync");
+const translators = require("../../constants/translator");
 
-const IMDBScraper = require('imdb-scraper');
+const IMDBScraper = require("imdb-scraper");
 const Imdb = new IMDBScraper({ requestDefaults: {}, maxRetries: 3 });
 
 /*
@@ -14,11 +14,11 @@ const Imdb = new IMDBScraper({ requestDefaults: {}, maxRetries: 3 });
 
 exports.getID = catchAsync(async (req, res, next) => {
     const body = new FormData();
-    body.append('q', req.query.search);
+    body.append("q", req.query.search);
 
     var config = {
-        method: 'post',
-        url: 'https://rezka.ag/engine/ajax/search.php',
+        method: "post",
+        url: "https://rezka.ag/engine/ajax/search.php",
         headers: {
             ...body.getHeaders(),
         },
@@ -30,9 +30,9 @@ exports.getID = catchAsync(async (req, res, next) => {
     let str = data.data;
 
     // extract movie id from html
-    str = str.slice(str.search('http'));
-    str = str.slice(0, str.search('>') - 1);
-    const id = str.slice(str.lastIndexOf('/') + 1, str.search('-'));
+    str = str.slice(str.search("http"));
+    str = str.slice(0, str.search(">") - 1);
+    const id = str.slice(str.lastIndexOf("/") + 1, str.search("-"));
 
     res.status(200).json({
         success: true,
@@ -42,19 +42,19 @@ exports.getID = catchAsync(async (req, res, next) => {
 
 exports.getSources = catchAsync(async (req, res, next) => {
     const body = new FormData();
-    body.append('translator_id', req.body.translator_id);
-    body.append('id', req.body.id);
+    body.append("translator_id", req.body.translator_id);
+    body.append("id", req.body.id);
 
     if (req.body.isSerial) {
-        body.append('action', 'get_stream');
-        body.append('season', req.body.season);
-        body.append('episode', req.body.episode);
+        body.append("action", "get_stream");
+        body.append("season", req.body.season);
+        body.append("episode", req.body.episode);
     } else {
-        body.append('action', 'get_movie');
+        body.append("action", "get_movie");
     }
 
     var config = {
-        method: 'post',
+        method: "post",
         url: `https://rezka.ag/ajax/get_cdn_series/?t=${new Date().getTime()}`,
         headers: {
             ...body.getHeaders(),
@@ -66,14 +66,14 @@ exports.getSources = catchAsync(async (req, res, next) => {
 
     let { url, subtitle } = data.data;
 
-    url = url.split(',');
+    url = url.split(",");
     let sources = [];
     for (let item of url) {
         let source = {
             quality: item
                 .match(/\[+(.*?)\]+/g)[0]
-                .replace(/\[+(.*?)\]+/g, '$1'),
-            url: item.slice(item.search('or ') + 3),
+                .replace(/\[+(.*?)\]+/g, "$1"),
+            url: item.slice(item.search("or ") + 3),
         };
         sources.push(source);
     }
@@ -102,11 +102,11 @@ exports.getIMDbInfo = catchAsync(async (req, res, next) => {
 
 exports.getAllAvailableTranslators = catchAsync(async (req, res, next) => {
     const body = new FormData();
-    body.append('q', req.query.title);
+    body.append("q", req.query.title);
 
     var config = {
-        method: 'post',
-        url: 'https://rezka.ag/engine/ajax/search.php',
+        method: "post",
+        url: "https://rezka.ag/engine/ajax/search.php",
         headers: {
             ...body.getHeaders(),
         },
@@ -116,19 +116,19 @@ exports.getAllAvailableTranslators = catchAsync(async (req, res, next) => {
     let data = await axios(config);
     let $ = cheerio.load(data.data);
 
-    let links = $('a');
+    let links = $("a");
     data = await axios.get(links[0].attribs.href);
 
     let list_data;
     let result = [];
     $ = cheerio.load(data.data);
-    $('ul[id=translators-list]')
-        .find('li')
+    $("ul[id=translators-list]")
+        .find("li")
         .each((index, element) => {
             list_data = $(element).attr();
             result.push({
                 name: list_data.title,
-                translator_id: list_data['data-translator_id'],
+                translator_id: list_data["data-translator_id"],
             });
         });
 
@@ -136,19 +136,20 @@ exports.getAllAvailableTranslators = catchAsync(async (req, res, next) => {
         var match = data.data.match(
             /sof.tv.initCDNMoviesEvents\((.+)\, (.+)\, 'rezka.ag'/
         );
-        let country = 'Country: ';
-        $('table[class=b-post__info]')
-            .find('td')
+        let country = "Country: ";
+        $("table[class=b-post__info]")
+            .find("td")
             .each((index, element) => {
                 if (index == 5) {
                     country += $(element).text();
                 }
             });
         if (match && match.length && match[2]) {
+            const something = translators.find(
+                (el) => el.translator_id == match[2]
+            );
             result.push({
-                name:
-                    translators.find((el) => el.translator_id == match[2])
-                        ?.name || country,
+                name: something ? something.name : country,
                 translator_id: match[2],
             });
         }
