a9ba5d5781df0da38eade882443aba475812b1d4
Nuriddin Muhammadjanov <nmuhammadjanov@gmail.com>
2021-03-20 16:01:39 +0500

    v2 mapped to v3

--- routes/v3/index.js
+++ routes/v3/index.js
@@ -5,8 +5,21 @@ const docs = require('../../docs/v3');
 const hdrezka = require('./hdrezka');
 const notifications = require('./notifications');
 
+const actor = require('../v2/actors');
+const movie = require('../v2/movies');
+const search = require('../v2/search');
+const category = require('../v2/categories');
+const collection = require('../v2/collection');
+
 router.use('/hdrezka', hdrezka);
 router.use('/notifications', notifications);
+
+router.use('/movies', movie);
+router.use('/categories', category);
+router.use('/actors', actor);
+router.use('/collections', collection);
+router.use('/hdrezka', hdrezka);
+router.use('/search', search);
 
 // DOCS
 router.use('/docs', swagger.serve);
